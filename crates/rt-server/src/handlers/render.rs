8e3c2d7627747f1ffb097d8dbf81bf38717dd5b4
Juan Esteban Guevara <juguevara@unal.edu.co>
2026-06-08 22:44:45 -0500

    World is now part of the state

--- crates/rt-server/src/handlers/render.rs
+++ crates/rt-server/src/handlers/render.rs
@@ -1,9 +1,7 @@
 use std::sync::{Arc, atomic::Ordering};
 
 use axum::{Json, extract::State, http::StatusCode};
-use rt_core::{Point3, Vec3};
 use rt_renderer::{render::render_scene, tracers::PathTracer};
-use rt_scene::{geometry::Sphere, hittable_list::HittableList, materials::{Dielectric, Lambertian, Metal}};
 
 use crate::{handlers::ErrorResponse, state::AppState};
 
@@ -45,21 +43,20 @@ pub async fn post_render(
         },
     }
 
-    let material_main = Arc::new(Lambertian::new(Vec3::new(1.0, 0.0, 0.0)));
-    let material_2 = Arc::new(Metal::new(Vec3::new(1.0, 1.0, 1.0), 0.0));
-    let material_3 = Arc::new(Metal::new(Vec3::new(0.0, 0.4, 0.8), 0.6));
-    let material_4 = Arc::new(Dielectric::new(1.5));
-    let material_5 = Arc::new(Dielectric::new(1.0/1.5));
-    let material_ground = Arc::new(Lambertian::new(Vec3::new(0.0, 0.9, 0.2)));
+    let world;
 
-    let mut world = HittableList::new();
+    match state.world.read() {
+        Ok(world_lock) => {
+            world = Arc::clone(&*world_lock)
+        },
+        Err(_) => {
+            let error_body = Json(ErrorResponse{
+                error: "The global camera lock hs suffered a poisoning".to_string(),
+            });
+            return Err((StatusCode::INTERNAL_SERVER_ERROR, error_body));
+        },
+    }
 
-    world.add(Arc::new(Sphere::new(Point3::new(-1.3, 0.0, -1.4), 0.5, material_2)));
-    world.add(Arc::new(Sphere::new(Point3::new(1.3, 0.0, -1.8), 0.5, material_3)));
-    world.add(Arc::new(Sphere::new(Point3::new(0.0, 0.8, -1.4), 0.5, material_4)));
-    world.add(Arc::new(Sphere::new(Point3::new(0.0, 0.8, -1.4), 0.1, material_5)));
-    world.add(Arc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_main)));
-    world.add(Arc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
 
     let fb_worker = Arc::clone(&state.framebuffer);
     let tx_worker = state.tx_stream.clone();
@@ -70,7 +67,7 @@ pub async fn post_render(
     tokio::task::spawn_blocking(move || {
         println!("¡Motor de renderizado incializado!");
         is_finished_worker.store(false, std::sync::atomic::Ordering::SeqCst);
-        render_scene(camera, tracer, fb_worker, tx_worker, payload.tile_size.unwrap_or(128), 3, &world);
+        render_scene(camera, tracer, fb_worker, tx_worker, payload.tile_size.unwrap_or(128), 3, &*world);
         is_finished_worker.store(true, std::sync::atomic::Ordering::SeqCst);
     });
 
--- crates/rt-server/src/state.rs
+++ crates/rt-server/src/state.rs
@@ -1,5 +1,7 @@
 use std::sync::{Arc, RwLock, atomic::AtomicBool};
+use rt_core::{Point3, Vec3};
 use rt_renderer::{camera::Camera, framebuffer::FrameBuffer};
+use rt_scene::{Hittable, geometry::Sphere, hittable_list::HittableList, materials::{Dielectric, Lambertian, Metal}};
 use tokio::sync::broadcast;
 
 #[derive(Clone)]
@@ -7,7 +9,8 @@ pub struct AppState {
     pub framebuffer: Arc<FrameBuffer>,
     pub tx_stream: broadcast::Sender<rt_renderer::tiles::TileResult>,
     pub is_finished: Arc<std::sync::atomic::AtomicBool>,
-    pub camera: Arc<RwLock<Arc<Camera>>>
+    pub camera: Arc<RwLock<Arc<Camera>>>,
+    pub world: Arc<RwLock<Arc<dyn Hittable + Send + Sync>>>,
 }
 
 impl AppState {
@@ -16,9 +19,26 @@ impl AppState {
         let framebuffer = Arc::new(FrameBuffer::new(camera.width, camera.height, stride));
         let (tx_stream, _) = broadcast::channel(n_channels);
         let is_finished = Arc::new(AtomicBool::new(true));
+        let mut world = HittableList::new();
+
+        let material_main = Arc::new(Lambertian::new(Vec3::new(1.0, 0.0, 0.0)));
+        let material_2 = Arc::new(Metal::new(Vec3::new(1.0, 1.0, 1.0), 0.0));
+        let material_3 = Arc::new(Metal::new(Vec3::new(0.0, 0.4, 0.8), 0.6));
+        let material_4 = Arc::new(Dielectric::new(1.5));
+        let material_5 = Arc::new(Dielectric::new(1.0/1.5));
+        let material_ground = Arc::new(Lambertian::new(Vec3::new(0.0, 0.9, 0.2)));
+
+
+        world.add(Arc::new(Sphere::new(Point3::new(-1.3, 0.0, -1.4), 0.5, material_2)));
+        world.add(Arc::new(Sphere::new(Point3::new(1.3, 0.0, -1.8), 0.5, material_3)));
+        world.add(Arc::new(Sphere::new(Point3::new(0.0, 0.8, -1.4), 0.5, material_4)));
+        world.add(Arc::new(Sphere::new(Point3::new(0.0, 0.8, -1.4), 0.1, material_5)));
+        world.add(Arc::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_main)));
+        world.add(Arc::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
 
         let camera = Arc::new(RwLock::new(camera));
+        let world = Arc::new(RwLock::new(Arc::new(world) as Arc<dyn Hittable + Send + Sync>));
 
-        Self { framebuffer, tx_stream, is_finished, camera }
+        Self { framebuffer, tx_stream, is_finished, camera , world }
     }
 }
